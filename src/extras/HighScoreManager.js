2d0e22562f6d1a0b27a386f8a8ad4818aa37dc88
Cristian Mircea Messel <mess110@gmail.com>
2018-12-02 19:13:25 +0200

    cleanup HighScoreManager

--- src/extras/HighScoreManager.js
+++ src/extras/HighScoreManager.js
@@ -1,40 +1,41 @@
 /*
+ * Manages high scores
+ *
+ * @example
+ *   # To register a new user/game
+ *   jNorthPole.createUser('api_key', 'secret')
+ *
+ * @example
+ *   hsm = HighScoreManager.get()
+ *   hsm.api_key = 'api_key'
+ *   hsm.secret = 'secret'
+ *
+ *   hsm.addScore('kiki', 210)
+ *   hsm.getScores(20) # highest 20 scores
+ *
+ *   hsm.responseHandler = (data) ->
+ *     # do something else
+ *
+ *
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
  * DS206: Consider reworking classes to avoid initClass
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-// @nodoc
 var HighScoreManager = (function() {
   let instance = undefined;
   HighScoreManager = class HighScoreManager {
     static initClass() {
-  
+
       instance = null;
-  
-      // Manages high scores
-      //
-      // @example
-      //   # To register a new user/game
-      //   jNorthPole.createUser('api_key', 'secret')
-      //
-      // @example
-      //   hsm = HighScoreManager.get()
-      //   hsm.api_key = 'api_key'
-      //   hsm.secret = 'secret'
-      //
-      //   hsm.addScore('kiki', 210)
-      //   hsm.getScores(20) # highest 20 scores
-      //
-      //   hsm.responseHandler = (data) ->
-      //     # do something else
+
       const Cls = (Singleton.HighScoreManager = class HighScoreManager {
         static initClass() {
           this.prototype.apiKey = 'guest';
           this.prototype.secret = 'guest';
         }
-  
+
         // Set apiKey and secret and attempt to register if tryRegister is true
         //
         // @param [String] apiKey
@@ -49,7 +50,7 @@ var HighScoreManager = (function() {
           this._ensureTokenPresence();
           return this;
         }
-  
+
         // Set apiKey and secret
         //
         // @param [String] apiKey
@@ -58,7 +59,7 @@ var HighScoreManager = (function() {
           this.apiKey = apiKey;
           return this.secret = secret;
         }
-  
+
         // add a score
         //
         // @param [String] name
@@ -67,7 +68,7 @@ var HighScoreManager = (function() {
           this._ensureTokenPresence();
           if (name == null) { throw new Error('name required'); }
           if (!isNumeric(score)) { throw new Error('score needs to be a number'); }
-  
+
           const json = {
             api_key: this.apiKey,
             secret: this.secret,
@@ -77,7 +78,7 @@ var HighScoreManager = (function() {
           };
           return jNorthPole.createStorage(json, this.responseHandler, this.errorHandler);
         }
-  
+
         // get scores
         //
         // @param [Number] limit
@@ -96,17 +97,17 @@ var HighScoreManager = (function() {
           };
           return jNorthPole.getStorage(json, this.responseHandler, this.errorHandler);
         }
-  
+
         // override this
         responseHandler(data) {
           return console.log(data);
         }
-  
+
         // override this
         errorHandler(data, status) {
           return console.log(data);
         }
-  
+
         _ensureTokenPresence() {
           if (this.apiKey == null) { throw new Error('apiKey missing'); }
           if (this.secret == null) { throw new Error('secret missing'); }
