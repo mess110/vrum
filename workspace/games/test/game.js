72968a34a24b4b3506d116ed28e7dfaea872fd06
Cristian Mircea Messel <mess110@gmail.com>
2019-03-25 13:28:31 +0200

    test use LoadingScene

--- workspace/games/test/game.js
+++ workspace/games/test/game.js
@@ -35,11 +35,39 @@ const switchScene = (event) => {
   Engine.switch(Hodler.get(sceneKey))
 }
 
-Hodler.add('scene1', new Scene1())
-Hodler.add('scene2', new Scene2())
-Hodler.add('scene3', new Scene3())
-Hodler.add('scene4', new Scene4())
-var loadingScene = new LoadingScene()
+let scene1 = new Scene1()
+let scene2 = new Scene2()
+let scene3 = new Scene3()
+let scene4 = new Scene4()
+let loadingScene = new LoadingScene(scene1, [
+  { type: 'font',  path: '/workspace/assets/fonts/luckiest-guy' },
+  { type: 'model', path: '/workspace/assets/models/chicken.gltf' },
+  { type: 'model', path: '/workspace/assets/models/chicken.gltf' },
+  { type: 'model', path: '/workspace/assets/models/panda.glb' },
+  { type: 'image', path: '/workspace/assets/models/chicken.png' },
+  { type: 'image', path: '/workspace/assets/textures/chicken_black.jpeg' },
+  { type: 'image', path: '/workspace/assets/textures/hand.png' },
+  { type: 'image', path: '/workspace/assets/textures/heightmap3.png' },
+  { type: 'image', path: '/workspace/assets/textures/waternormals.jpg' },
+  { type: 'image', path: '/workspace/assets/textures/smokeparticle.png' },
+  { type: 'image', path: '/workspace/assets/textures/sprite-explosion2.png' },
+  { type: 'image', path: '/workspace/assets/textures/black-faded-border.png' },
+  { type: 'image', path: '/workspace/assets/textures/vrum.png' },
+  { type: 'image', path: '/workspace/assets/textures/grass.png' },
+  { type: 'json',  path: '/workspace/assets/particles/particle.json' },
+  { type: 'json',  path: '/workspace/assets/graffiti/majestic-frog-cover.json' },
+  { type: 'json',  path: '/workspace/assets/shaders/basic_shader.json' },
+  { type: 'json',  path: '/workspace/assets/shaders/dissolve_shader.json' },
+  { type: 'json',  path: '/workspace/assets/terrains/terrain.json' },
+  { type: 'json',  path: '/workspace/assets/scenes/boat-scene.json' },
+  { type: 'sound', path: '/workspace/assets/sounds/hit.wav' },
+  { type: 'sound', path: '/workspace/assets/sounds/SuperHero_original.ogg' },
+])
+
+Hodler.add('scene1', scene1)
+Hodler.add('scene2', scene2)
+Hodler.add('scene3', scene3)
+Hodler.add('scene4', scene4)
 
 Persist.default('name', 'player1')
 console.log(Persist.get('name'))
