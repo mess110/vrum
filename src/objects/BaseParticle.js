395fa69476b6ccb6c58f20e1f285297996bd0704
Cristian Mircea Messel <mess110@gmail.com>
2019-04-08 02:05:09 +0300

    add particles

--- src/objects/BaseParticle.js
+++ src/objects/BaseParticle.js
@@ -6,19 +6,46 @@ class BaseParticle extends THREE.Object3D {
 
     let jsonInput;
     this.groups = [];
+    this.emitters = [];
 
     eval(`jsonInput = ${input}`);
     if (jsonInput == null) { jsonInput = []; }
+    this.jsonInput = jsonInput
 
     for (let json of Array.from(jsonInput)) {
       const group = new (SPE.Group)(json);
       for (let emitJson of Array.from(json.emitters)) {
         const emitter = new (SPE.Emitter)(emitJson);
         group.addEmitter(emitter);
+        this.emitters.push(emitter)
       }
       this.groups.push(group);
       this.add(group.mesh);
     }
+  }
+
+  getMaxAge() {
+    let ages = []
+    this.jsonInput.forEach((json) => {
+      json.emitters.forEach((e) => {
+        let age = isBlank(e.maxAge) || isBlank(e.maxAge.value) ? 2 : e.maxAge.value
+        ages.push(age)
+      })
+    })
+    let max = Math.max(...ages)
+    return max
+  }
+
+  enable() {
+    this.emitters.forEach((e) => {
+      e.enable()
+    })
+  }
+
+  disable() {
+    this.emitters.forEach((e) => {
+      e.disable()
+    })
   }
 
   // Used to animate the particle
